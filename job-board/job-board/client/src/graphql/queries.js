c539602dcd65af02d573ace1d161c1344029ab0a
jirka <jorgetimes2@gmail.com>
2022-09-08 14:23:31 +0200

    GraphQL-Request Client

---
+++ job-board/job-board/client/src/graphql/queries.js
@@ -0,0 +1,19 @@
+import { request, gql } from 'graphql-request';
+
+const GRAPHQL_URL = 'http://localhost:9999/graphql';
+
+export async function getJobs() {
+    const query = gql`
+        query {
+            jobs {
+                title
+                id
+                company {
+                    name
+                }
+            }
+        }
+    `;
+    const data = await request(GRAPHQL_URL, query);
+    console.log('data: ', data);
+}
