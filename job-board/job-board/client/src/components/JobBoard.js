996aff8925621ed797613aef01784bc6aefaa12d
jirka <jorgetimes2@gmail.com>
2022-09-08 16:19:46 +0200

    error handling

--- job-board/job-board/client/src/components/JobBoard.js
+++ job-board/job-board/client/src/components/JobBoard.js
@@ -4,19 +4,25 @@ import {useEffect, useState} from "react";
 
 function JobBoard() {
     const [jobs, setJobs] = useState([]);
+    const [error, setError] = useState(false);
     useEffect(() => {
-        getJobs().then(setJobs);
+        getJobs().then(setJobs)
+            .catch((err) => setError(true));
     }, []);
 
     console.log('[JobBoard] jobs: ', jobs);
-  return (
+
+    if (error) {
+        return <p>Sorry, something went wrong.</p>;
+    }
+    return (
     <div>
       <h1 className="title">
         Job Board
       </h1>
       <JobList jobs={jobs} />
     </div>
-  );
+    );
 }
 
 export default JobBoard;
