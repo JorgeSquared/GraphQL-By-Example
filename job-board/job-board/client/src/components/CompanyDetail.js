7bdf386f7a640dc75436cbd3c792311c5ed07b6e
jirka <jorgetimes2@gmail.com>
2022-09-08 15:24:17 +0200

    exercise: company query

--- job-board/job-board/client/src/components/CompanyDetail.js
+++ job-board/job-board/client/src/components/CompanyDetail.js
@@ -1,11 +1,18 @@
 import { useParams } from 'react-router';
-import { companies } from '../fake-data';
+import {getCompany} from "../graphql/queries";
+import {useEffect, useState} from "react";
 
 function CompanyDetail() {
-  const { companyId } = useParams();
+    const [company, setCompany] = useState(null);
+    const { companyId } = useParams();
+    useEffect(() => {
+        getCompany(companyId).then(setCompany);
+    }, [companyId]);
 
-  const company = companies.find((company) => company.id === companyId);
-  return (
+    if (!company) {
+        return <p>Loading...</p>;
+    }
+    return (
     <div>
       <h1 className="title">
         {company.name}
@@ -14,7 +21,7 @@ function CompanyDetail() {
         {company.description}
       </div>
     </div>
-  );
+    );
 }
 
 export default CompanyDetail;
